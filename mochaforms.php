38f25aedf8ac0365a6843099b46056bed1fe65de
Monkey <monkey@dev.local>
2021-08-28 11:30:51 -0700

    Adds basic WP description

---
+++ mochaforms.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *  @package MochaForms
+ */
+/*
+Plugin Name: MochaForms
+Plugin URI: http://mochaforms.com/plugin
+Description: A complete form generator solution for wordpress focused on simple setup.
+
+
+
+
+
+
+ */
