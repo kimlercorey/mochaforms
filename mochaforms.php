c21c7f464b1f56a429fe269a8bd943ed2bbde272
kimler <kimler@gmail.com>
2021-08-28 11:54:41 -0700

    Extends additional information to plugin

--- mochaforms.php
+++ mochaforms.php
@@ -6,10 +6,10 @@
 Plugin Name: MochaForms
 Plugin URI: http://mochaforms.com/plugin
 Description: A complete form generator solution for wordpress focused on simple setup.
-
-
-
-
-
-
- */
+Version: 1.0.0
+Author: MochaForms Team
+Author URI: http://mochaforms.com/team
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: mochaforms
+*/
