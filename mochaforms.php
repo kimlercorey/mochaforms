2caa74883d4627d2e4f0f4331ebeeeef614342dd
kimler <kimler@gmail.com>
2021-08-28 12:00:56 -0700

    Extends additional links towq plugin

--- mochaforms.php
+++ mochaforms.php
@@ -12,4 +12,21 @@ Author URI: http://mochaforms.com/team
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: mochaforms
+ */
+
+
+
+/**
+* Plugin action links
+*
+* @param array $links
+*
+* @return array
 */
+
+public function plugin_action_links( $links ) {
+    $links[] = '<a href="https://mochaforms.com/?utm_source=mochaforms-action-link&utm_medium=textlink&utm_campaign=plugin-docs-link" target="_blank">' . __( 'Docs', 'mochaforms' ) . '</a>';
+    $links[] = '<a href="' . admin_url( 'admin.php?page=mochaforms' ) . '">' . __( 'Settings', 'mochaforms' ) . '</a>';
+
+    return $links;
+}
