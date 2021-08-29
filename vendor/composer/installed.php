abc1ab558cae615506d3381ee01acd90a541c6ed
kimler <kimler@gmail.com>
2021-08-28 19:28:37 -0700

    adds vendor for autoload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '7ed4779fbeec5709cbc6b31368f3364fb7d91043',
+        'name' => 'mochaforms/mochaforms',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mochaforms/mochaforms' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '7ed4779fbeec5709cbc6b31368f3364fb7d91043',
+            'dev_requirement' => false,
+        ),
+    ),
+);
